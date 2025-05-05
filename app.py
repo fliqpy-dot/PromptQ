d2c64c6c675ad349c8c0caa161af8f8fd8ae80f6
Venus Bradley D. <your@email.com>
2025-05-05 14:42:58 +0800

    Fixed the port and host for production

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ def get_current_number():
     return jsonify(current_number)
 
 if __name__ == "__main__":
-    app.run(debug=True)
- 
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local development, Railway will provide its port
+    app.run(host="0.0.0.0", port=port, debug=True)
